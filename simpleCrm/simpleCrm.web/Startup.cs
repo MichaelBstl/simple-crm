0065884e70e1426bb292c72ef84d2cdd9a7b2fe1
Michael L Bryant <MichaelB_1968@yahoo.com>
2022-03-08 11:01:20 -0600

    Step 004: using configuration

--- simpleCrm/simpleCrm.web/Startup.cs
+++ simpleCrm/simpleCrm.web/Startup.cs
@@ -7,11 +7,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace simpleCrm.web
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -32,7 +38,8 @@ namespace simpleCrm.web
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World1!");
+                    var greeting = configuration["Greeting"];
+                    await context.Response.WriteAsync(greeting);
                 });
             });
         }
