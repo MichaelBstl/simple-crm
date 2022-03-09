bca35f878ebd8ac103aff919f7aca0caca358656
Michael L Bryant <MichaelB_1968@yahoo.com>
2022-03-09 09:09:36 -0600

    CLEANUP

--- simpleCrm/SimpleCrm/ConfigurationGreeter.cs
+++ simpleCrm/SimpleCrm/ConfigurationGreeter.cs
@@ -7,13 +7,14 @@ namespace SimpleCrm
 {
     public class ConfigurationGreeter : IGreeter
     {
+        public IConfiguration Configuration { get; }
         public ConfigurationGreeter(IConfiguration configuration)
         {
-
+            Configuration = configuration;
         }
         public string GetGreeting()
         {
-            return "new greeting from GetGreeting";
+            return Configuration["Greeting"];
         }
     }
 }
--- simpleCrm/simpleCrm.web/Startup.cs
+++ simpleCrm/simpleCrm.web/Startup.cs
@@ -14,10 +14,11 @@ namespace SimpleCrm.web
     public class Startup
     {
         private readonly IConfiguration configuration;
-        public Startup(IConfiguration configuration)
+/*        public Startup(IConfiguration configuration)
         {
             this.configuration = configuration;
         }
+*/
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
