8d71abd5f65abff682481ec95c408ada5dd1aea9
Michael L Bryant <MichaelB_1968@yahoo.com>
2022-03-18 12:58:43 -0500

    step34

---
+++ simpleCrm/simpleCrm.web/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleCrm.web.Models.Account;
+
+
+namespace SimpleCrm.web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new RegisterUserViewModel
+                {
+                    UserName = model.UserName,
+                    DisplayName = model.DisplayName,
+                    Password = model.Password,
+                    ConfirmPassword = model.ConfirmPassword
+                };
+
+                return View();
+            }
+            return View();
+        }
+    }
+}
---
+++ simpleCrm/simpleCrm.web/Models/Account/RegisterUserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCrm.web.Models.Account
+{
+    public class RegisterUserViewModel
+    {
+        [Required, MaxLength(256)] // TODO: Set annotation attributes in the assignment
+        public string UserName { get; set; }
+        [Required, MaxLength(256), DisplayName("Name")]
+        public string DisplayName { get; set; }
+        [Required, MinLength(6), DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required, MinLength(6), Compare("Password", ErrorMessage = "The Passwords do not match"), DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
