d16dfb48399ce76f4fcf7636995cf18b88c5c067
Michael L Bryant <MichaelB_1968@yahoo.com>
2022-03-29 10:25:17 -0500

    51: add angular material and layout

--- simple-crm-cli/src/app/app.module.ts
+++ simple-crm-cli/src/app/app.module.ts
@@ -4,13 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule} from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
