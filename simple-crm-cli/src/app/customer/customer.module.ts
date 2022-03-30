3b1573c0a592ff7e7254dc7b66ebb9a1a52e423e
Michael L Bryant <MichaelB_1968@yahoo.com>
2022-03-30 18:03:51 -0500

    54: customer list table on a card, with a max cont

--- simple-crm-cli/src/app/customer/customer.module.ts
+++ simple-crm-cli/src/app/customer/customer.module.ts
@@ -4,11 +4,18 @@ import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerListPageComponent } from './customer-list-page/customer-list-page.component';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+
 @NgModule({
   declarations: [CustomerListPageComponent],
   imports: [
     CommonModule,
-    CustomerRoutingModule
+    CustomerRoutingModule,
+    MatCardModule,
+    MatTableModule,
+    MatSortModule
   ]
 })
 export class CustomerModule { }
