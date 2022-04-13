2f9f5bc5f0adf811de9e4be8a2c9317b7dcd120c
Michael L Bryant <MichaelB_1968@yahoo.com>
2022-04-13 09:25:08 -0500

    68: unit testing app component

--- simple-crm-cli/src/app/app.component.spec.ts
+++ simple-crm-cli/src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -20,16 +20,39 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'simple-crm-cli'`, () => {
+  it(`should have as title 'Simple CRM'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('simple-crm-cli');
+    expect(app.title).toEqual('Simple CRM');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('simple-crm-cli app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('span.title').textContent).toContain('Simple CRM');
+  });
+
+  it('menu item Customers exists', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h4').textContent).toContain('Customers');
+  });
+
+  it('menu item Pipeline exists', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+
+    const menuItems = compiled.querySelectorAll('h4');
+    var containsElement:Boolean = false;
+
+    if (menuItems[1].innerHTML.search(/pipeline/i) === 0)
+    {
+      containsElement = true;
+    }
+
+    expect(containsElement).toEqual(true);
   });
 });
