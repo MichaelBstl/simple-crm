b8b5e407c0bb93cec6c8bea7ba903eab6bc1bc95
Michael L Bryant <MichaelB_1968@yahoo.com>
2022-04-14 12:06:01 -0500

    104: sql getbystatus - secure orderby

--- simpleCrm/SimpleCrm.sqlDbServices/SqlCustomerData.cs
+++ simpleCrm/SimpleCrm.sqlDbServices/SqlCustomerData.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Linq.Dynamic.Core;
 namespace SimpleCrm.SqlDbServices
 {
     public class SqlCustomerData : ICustomerData
@@ -29,10 +29,67 @@ namespace SimpleCrm.SqlDbServices
         }
         public void Update(Customer customer)
         {
+            //Update is not needed, since changes are tracked by EF
         }
         public void Commit()
         {
             dbContext.SaveChanges();
+        }
+
+        public List<Customer> GetByStatus(CustomerStatus status, int pageIndex, int take, string orderBy)
+        {
+            //valid columns to sort by
+            var validColumns = new string[] { "Id", "FirstName", "LastName", "PhoneNumber", "OptInNewsletter", "Type", "EmailAddress", "ContactMethod", "Status", "LastContactDate" }.ToList();
+            // default to last name sort order if orderBy is blank or null
+            if (string.IsNullOrWhiteSpace(orderBy))
+            {
+                orderBy = "LastName";
+            }
+            var splitOrderBy = orderBy.Split(',');
+            // examine each sort parameter & make sure it has the proper number of parameters & the parameters are 
+            //   valid columns & sort direction
+            foreach(string order in splitOrderBy)
+            {
+                var component = order.Split(" ");
+                if (component.Length == 1 || component.Length == 2)
+                { //valid as it has one or two arguments
+                }
+                else
+                {
+                    throw new Exception("Invalid sort parameter. Incorrect number of arguments:" + order);
+                }
+                if (!validColumns.Contains(component[0].ToLower()))
+                {
+                    throw new Exception("Invalid sort parameter. Incorrect column specified:" + component);
+                }
+
+
+                if (!(component[1].ToLower().Contains("asc") || 
+                     component[1].ToLower().Contains("desc") ||
+                     string.IsNullOrWhiteSpace(component[1])
+                     ))
+                {
+                    throw new Exception("Invalid sort parameter. Incorrect sort order:" + component);
+                }
+            }
+
+
+            return dbContext.Customers.Where(x => x.Status == status)
+                .OrderBy(orderBy)
+                .Skip(pageIndex * take)
+                .Take(take).ToList();
+            
+        }
+
+        public void Delete(Customer customer)
+        {
+            dbContext.Remove(customer);
+        }
+        public void Delete(int id)
+        {
+            var cust = new Customer { Id = id };
+            dbContext.Attach(cust);
+            dbContext.Remove(cust);
         }
     }
 }
