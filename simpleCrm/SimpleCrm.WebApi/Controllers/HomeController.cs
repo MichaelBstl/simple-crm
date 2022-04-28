bb1564a9d8094b08163c5f8a0ee0ef762228528f
Michael L Bryant <MichaelB_1968@yahoo.com>
2022-04-28 11:04:20 -0500

    112: marketing page content

--- simpleCrm/SimpleCrm.WebApi/Controllers/HomeController.cs
+++ simpleCrm/SimpleCrm.WebApi/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace SimpleCrm.WebApi.Controllers
 {
+    [Route("")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -17,17 +18,47 @@ namespace SimpleCrm.WebApi.Controllers
         {
             _logger = logger;
         }
-
+        [Route("")]
         public IActionResult Index()
         {
             return View();
         }
-
+        [Route("Privacy")]
         public IActionResult Privacy()
         {
+            ViewData["Message"] = "Your application privacy page.";
+
             return View();
         }
 
+        [Route("about")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Route("contact")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+        [Route("firstpage")]
+        public IActionResult FirstPage()
+        {
+            ViewData["Message"] = "This is my first marketing page.";
+            return View();
+        }
+        [Route("secondpage")]
+        public IActionResult SecondPage()
+        {
+            ViewData["Message"] = "This is my second marketing page.";
+            return View();
+        }
+        [Route("error")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
