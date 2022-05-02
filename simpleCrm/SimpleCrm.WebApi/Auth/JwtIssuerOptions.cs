0799511b7014d14b85efe98732704fa7376d1282
Michael L Bryant <MichaelB_1968@yahoo.com>
2022-05-02 09:42:54 -0500

    115a: Add JWT custom options

---
+++ simpleCrm/SimpleCrm.WebApi/Auth/JwtIssuerOptions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+
+namespace SimpleCrm.WebApi.Auth
+{
+    public class JwtIssuerOptions
+    {
+        string Issue { get; set; }
+        string Subject { get; set; }
+        string Audience { get; set; }
+        DateTime Expiration { get; set; }
+        DateTime NotBefore { get; set; }
+        DateTime IssuedAt { get; set; }
+        SigningCredentials SigningCredentials { get; set; }
+    }
+}
